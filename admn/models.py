83275f870a4f95d9e94943d50da9f7968061a201
irfankpr <irfanmuhammedkpr@gmail.com>
2022-09-10 01:56:50 +0530

    migratopn problem solved in importing   validate.models to admn.models

--- admn/models.py
+++ admn/models.py
@@ -1,6 +1,3 @@
-from django.db import models
+from validate.models import users
 # Create your models here.
-class users(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    is_staff = models.BooleanField(null=False)
+users
